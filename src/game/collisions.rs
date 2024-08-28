4e0336b09646b5429cf2ea4eddca5d8547966c84
James Sizeland <65866907+jamessizeland@users.noreply.github.com>
2024-08-28 14:27:14 +0100

    add collision events for enemies too

--- src/game/collisions.rs
+++ src/game/collisions.rs
@@ -1,22 +1,16 @@
 //! Plugin to handle collisions between entities
 
-use bevy::{prelude::*, utils::hashbrown::Equivalent};
-use bevy_rapier2d::prelude::*;
+use super::{enemy::Enemy, events::ShipHit, player::Player};
+use crate::prelude::*;
+use bevy::utils::hashbrown::Equivalent;
 
-use crate::game::events::EndGameTriggered;
-
-use super::{enemy::Enemy, events::ShipHit, player::Player, state::InGameState};
-
-/// Plugin to handle collisions between entities
-pub struct CollisionsPlugin;
-
-impl Plugin for CollisionsPlugin {
-    fn build(&self, app: &mut App) {
-        app.add_systems(
-            PostUpdate,
-            collision_events.run_if(in_state(InGameState::Play)),
-        );
-    }
+pub(super) fn plugin(app: &mut App) {
+    app.add_systems(
+        PostUpdate,
+        collision_events
+            .in_set(AppSet::Update)
+            .run_if(in_state(InGameState::Playing)),
+    );
 }
 
 /// Trigger an end of game event when the player gets hit by an enemy
@@ -27,16 +21,26 @@ fn collision_events(
     players: Query<Entity, With<Player>>,
     enemies: Query<Entity, With<Enemy>>,
 ) {
-    let player = players.single();
+    let player = players
+        .get_single()
+        .expect("Collision system found more than one player, this should not happen");
+
     for collision_event in collision_events.read() {
         for enemy in enemies.iter() {
             if let CollisionEvent::Started(entity1, entity2, _) = collision_event {
-                if (entity1.equivalent(&player) && entity2.equivalent(&enemy))
-                    || (entity2.equivalent(&player) && entity1.equivalent(&enemy))
-                {
-                    info!("Player collided with enemy");
-                    player_destroyed.send(EndGameTriggered);
-                }
+                if entity1.equivalent(&enemy) {
+                    ship_hit_events.send(ShipHit { id: *entity1 });
+                    if entity2.equivalent(&player) {
+                        info!("Player collided with enemy");
+                        player_destroyed.send(EndGameTriggered);
+                    }
+                } else if entity2.equivalent(&enemy) {
+                    ship_hit_events.send(ShipHit { id: *entity2 });
+                    if entity1.equivalent(&player) {
+                        info!("Player collided with enemy");
+                        player_destroyed.send(EndGameTriggered);
+                    }
+                };
             }
         }
     }
