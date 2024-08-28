27b215de83dcc18c24576c22a0eb1d658f852a7f
James Sizeland <65866907+jamessizeland@users.noreply.github.com>
2024-08-28 14:26:01 +0100

    migrate app setup from main to lib

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,9 @@
-mod common;
-mod game;
-mod menu;
-mod window;
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
-use crate::common::better_button::BetterButtonPlugin;
-use bevy::input::common_conditions::input_toggle_active;
 use bevy::prelude::*;
-use bevy::text::TextSettings;
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
-use bevy_prototype_lyon::plugin::ShapePlugin;
-use window::UiPlugin;
+use chase_me::AppPlugin;
 
-#[derive(Debug, Clone, Copy, Default, Eq, PartialEq, Hash, States)]
-pub enum AppState {
-    #[default]
-    Menu,
-    InGame,
-    RestartInGame,
-}
-
-fn main() {
-    App::new()
-        .init_state::<AppState>()
-        .add_plugins(
-            DefaultPlugins
-                .set(ImagePlugin::default_nearest())
-                .set(WindowPlugin {
-                    primary_window: Some(Window {
-                        title: "Chase me!".into(),
-                        ..default()
-                    }),
-                    ..default()
-                }),
-        )
-        .insert_resource(Msaa::Sample4) // shape lyon
-        .insert_resource(TextSettings {
-            allow_dynamic_font_size: true,
-            ..default()
-        })
-        .add_plugins((
-            WorldInspectorPlugin::default().run_if(input_toggle_active(false, KeyCode::KeyI)),
-        ))
-        .add_plugins((
-            ShapePlugin, // shape lyon
-            BetterButtonPlugin,
-            game::GamePlugin,
-            menu::MenuPlugin,
-            UiPlugin,
-        ))
-        .run();
+fn main() -> AppExit {
+    App::new().add_plugins(AppPlugin).run()
 }
