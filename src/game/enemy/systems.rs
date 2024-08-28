431a8d5b34d63cc7aa68ff95a45af27569f3be64
James Sizeland <65866907+jamessizeland@users.noreply.github.com>
2024-08-28 14:30:56 +0100

    change spawning for centered coords

--- src/game/enemy/systems.rs
+++ src/game/enemy/systems.rs
@@ -1,6 +1,10 @@
 use super::{resources::EnemyStrengthRange, Enemy, EnemyParent, EnemyState, GameTime, Score};
-use crate::game::{events::ShipDestroyed, movement::Momentum, player::Player};
-use bevy::{prelude::*, window::PrimaryWindow};
+use crate::game::{
+    events::{ShipDestroyed, ShipHit},
+    movement::Momentum,
+    player::Player,
+};
+use bevy::{prelude::*, utils::hashbrown::Equivalent, window::PrimaryWindow};
 use bevy_prototype_lyon::prelude::*;
 use bevy_rapier2d::prelude::*;
 use rand::Rng;
@@ -24,11 +28,11 @@ pub fn spawn_enemy(
     let parent = parent.single();
     let player_transform = player.single();
     let Ok(window) = window.get_single() else {
-        warn!("no primary window, not implementing bounded movement");
+        warn!("no primary window, not spawning enemy");
         return;
     };
-    let width = window.resolution.width();
-    let height = window.resolution.height();
+    let half_width = window.resolution.width() / 2.0;
+    let half_height = window.resolution.height() / 2.0;
 
     // spawn enemies with different shapes, lifetimes, max speeds, max energies, recharge rates, masses and thrusts.  These are randomized but get more difficult as the game progresses.
     // The colour and shape of the enemy should be associated with the difficulty level of the spawned enemy's stats.
@@ -41,30 +45,30 @@ pub fn spawn_enemy(
     let radius = max_enemy_strength.get_radius(momentum.mass); // radius of the enemy
 
     assert!(
-        radius < width / 2.0 && radius < height / 2.0,
+        radius < half_width && radius < half_height,
         "Enemy radius is too large"
     );
     // spawn location, must be at least 20% of the window width away from the player and at least its own radius away from a wall.
     let mut rng = rand::thread_rng();
     let (random_x, random_y) = loop {
-        let random_x = rng.gen_range(0.0 + radius..=width - radius);
-        let random_y = rng.gen_range(0.0 + radius..=height - radius);
-        if random_x < player_transform.translation.x - 0.2 * width
-            || random_x > player_transform.translation.x + 0.2 * width
-            || random_y < player_transform.translation.y - 0.2 * height
-            || random_y > player_transform.translation.y + 0.2 * height
+        let random_x = rng.gen_range(-half_width + radius..=half_width - radius);
+        let random_y = rng.gen_range(-half_height + radius..=half_height - radius);
+        if random_x < player_transform.translation.x - 0.4 * half_width
+            || random_x > player_transform.translation.x + 0.4 * half_width
+            || random_y < player_transform.translation.y - 0.4 * half_height
+            || random_y > player_transform.translation.y + 0.4 * half_height
         {
             break (random_x, random_y);
         }
     };
-    info!("random_x: {}, random_y: {}", random_x, random_y);
+    // info!("random_x: {}, random_y: {}", random_x, random_y);
 
     // generate a colour and shape based on the difficulty level of the enemy, which is based on the enemy's stats. Red should be the easiest, scaling up to blue the hardest.
     let colour = max_enemy_strength.get_colour(&momentum, &enemy);
     enemy.colour = colour; // set to use for explosion colour on death
     let path = max_enemy_strength.get_shape(&momentum, &enemy);
 
-    info!("Spawning enemy with stats: {:?}", (&momentum, &enemy));
+    // info!("Spawning enemy with stats: {:?}", (&momentum, &enemy));
 
     commands
         .spawn((
@@ -86,7 +90,11 @@ pub fn spawn_enemy(
             RigidBody::Dynamic,
             Ccd::enabled(),
             GravityScale(0.0),
-            Velocity::default(),
+            Velocity {
+                linvel: Vec2::new(0.1, 0.1),
+                angvel: 2.0,
+            },
+            ActiveEvents::COLLISION_EVENTS,
             enemy,
         ))
         .set_parent(parent);
@@ -111,7 +119,7 @@ pub fn enemy_lifetime(
                 }
             }
             EnemyState::Moving => {
-                enemy.energy -= time_passed.clamp(0.0, enemy.energy);
+                enemy.energy = (enemy.energy - time_passed).clamp(0.0, enemy.max_energy);
                 enemy.lifetime.tick(time.delta());
                 if enemy.energy <= 0.0 {
                     enemy.state = EnemyState::Stopped;
@@ -127,6 +135,25 @@ pub fn enemy_lifetime(
                         colour: enemy.colour,
                     });
                 }
+            }
+        }
+    }
+}
+
+/// Enemies lose energy when they get hit
+pub fn enemy_hit(
+    mut objects: Query<(Entity, &mut Enemy, &Velocity)>,
+    mut hit_events: EventReader<ShipHit>,
+) {
+    for events in hit_events.read() {
+        for (entity, mut enemy, velocity) in objects.iter_mut() {
+            // read events and reduce energy of enemies.
+            if events.id.equivalent(&entity) {
+                info!(
+                    "enemy {} hit something at {} with {}/{} energy",
+                    events.id, velocity.linvel, enemy.energy, enemy.max_energy
+                );
+                enemy.energy -= enemy.max_energy * 0.2; // drop energy by 20% on collision
             }
         }
     }
